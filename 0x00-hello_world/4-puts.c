de71df35bec534afedce6565233601e8efc732df
HAMZA <hamabbas910@gmail.com>
2023-03-16 08:30:02 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/* we should print a long char on using puts */
-/* this programme must return the value of 0*/
+/**
+ * main = Entry
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
