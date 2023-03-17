38876af5ce4f54147f1414d5642309e71c1a5171
HAMZA <hamabbas910@gmail.com>
2023-03-17 14:23:38 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 if success
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[26]);
+	}
+	putchar('\n');
+	return (0);
+}
