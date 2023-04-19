bc351529cf62203a1e2276a9bdebc56291803cd0
HAMZA <hamabbas910@gmail.com>
2023-04-19 04:57:47 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,7 +8,7 @@
  * @argv: An array of pointers to the arguments
  * Return: Always 0
  */
-int main(__attribute__((__unused__)) int argc, char *argv[])
+int main(int __attribute__((__unused__))argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
